ab12c872e47e6ec222ed7f31975262dad106a105
KhalWalid <walidcav@gmail.com>
2021-03-03 10:18:38 +0100

    Can now click on the grid to add the X's and the O's

--- script.js
+++ script.js
@@ -1,44 +1,76 @@
-gameBoardContainer = document.querySelector('#gameBoard');
-squareDiv = document.createElement('div')
-let array = ['X', 'O', ' ']
-
-
 const gameBoard = (() => {
-    const gameBoardArray = []
-    return {
+    const gameBoardContainer = document.querySelector('#gameBoard');
+    const squareDiv = document.createElement('div');
+    squareDiv.id = 'squareDiv';
 
+    const gameBoardArray = ['', '', '', '', '', '', '', '', ''];
+
+    const changeSquare = (sign, index) => {
+        if (gameBoardArray[index] !== '') return;
+        gameBoardArray[index] = sign;
     };
-});
+
+    return {
+        gameBoardArray,
+        squareDiv,
+        gameBoardContainer,
+        changeSquare
+    };
+})();
+
+const gameController = (() => {
+    let round = 1
+
+    const playRound = (index) => {
+        gameBoard.changeSquare(getSign(), index);
+        round++
+    };
+
+    const getSign = () => {
+        return round % 2 == 0 ? 'O' : 'X';
+    };
+
+    return {
+        playRound
+    };
+})();
 
 const displayController = (() => {
+    const displayArray = () => {
+        gameBoard.gameBoardContainer.innerHTML = ''
+        gameBoard.gameBoardArray.forEach(element => {
+            switch (element) {
+                case 'X':
+                    gameBoard.squareDiv.innerHTML = '<p>X</p>';
+                    break;
+                case 'O':
+                    gameBoard.squareDiv.innerHTML = '<p>O</p>';
+                    break;
+                case '':
+                    gameBoard.squareDiv.innerHTML = '<p></p>';
+                    break;
+            }
+            gameBoard.gameBoardContainer.appendChild(gameBoard.squareDiv.cloneNode(true))
+        });
+
+        const squareDivs = document.querySelectorAll('#squareDiv')
+
+        squareDivs.forEach((square, index) => {
+            square.addEventListener('click', () => {
+                gameController.playRound(index)
+                displayArray()
+            });
+        });
+    }
+
+    displayArray()
 
     return {
-
     };
-});
+
+})();
 
 const Player = name => {
 
     return {};
 };
-
-function displayGrid() {
-    array.forEach(element => {
-        switch (element) {
-            case 'X':
-                squareDiv.innerHTML = '<p>X</p>';
-                break;
-            case 'O':
-                squareDiv.innerHTML = '<p>O</p>';
-                break;
-            case ' ':
-                squareDiv.innerHTML = '<p> </p>';
-                break;
-        }
-        console.log(squareDiv.innerHTML)
-        console.log(squareDiv.cloneNode().innerHTML)
-        gameBoardContainer.appendChild(squareDiv.cloneNode(true))
-    });
-};
-
-displayGrid()
